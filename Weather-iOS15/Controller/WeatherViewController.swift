6e3fa8eee2b96fbba3bb7d6486fd4c758765f244
Kaycee <KayceeT2>
2021-07-02 11:46:29 -0400

    Initial Commit

---
+++ Weather-iOS15/Controller/WeatherViewController.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
